3b6f41c639ea477eece08dce5def7b9093762937
lltphs <physicliveson@gnail.com>
2021-07-13 20:58:50 +0700

    more DAT-specific

--- src/registerLoaders.js
+++ src/registerLoaders.js
@@ -1,6 +1,6 @@
 import { loadImage } from './loadImage.js';
 
 export default function (cornerstone) {
-  // Register the http and https prefixes so we can use standard web urls directly
+  // Register empty prefixes
   cornerstone.registerImageLoader('', loadImage);
 }
